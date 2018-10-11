386d378b37223ca3229754fd453acf865482db66
mau-guerrero <smg10@rice.edu>
2018-10-10 19:39:17 -0500

    formats and adds docs to LEDToy

--- lib/LEDToy/src/LEDToy.cpp
+++ lib/LEDToy/src/LEDToy.cpp
@@ -1,17 +1,14 @@
 #include <LEDToy.h>
 
-LEDToy::LEDToy(byte pin_number)
-{
+LEDToy::LEDToy(byte pin_number) {
   pin = pin_number;
 }
 
-void LEDToy::begin()
-{
+void LEDToy::begin() {
   pinMode(pin, OUTPUT);
 }
 
-void LEDToy::flash(int frequency)
-{
+void LEDToy::flash(int frequency) {
   int delay_time = 1000 / frequency;
   digitalWrite(pin, HIGH);
   delay(delay_time / 2);
@@ -19,17 +16,12 @@ void LEDToy::flash(int frequency)
   delay(delay_time / 2);
 }
 
-void LEDToy::dim(int delay_time)
-{
-  for (int i = 0; i < 512; i++)
-  {
-    if (i < 256)
-    {
+void LEDToy::dim(int delay_time) {
+  for (int i = 0; i < 512; i++) {
+    if (i < 256) {
       analogWrite(pin, i);
       delay(delay_time);
-    }
-    else
-    {
+    } else {
       analogWrite(pin, 511 - i);
       delay(delay_time);
     }
--- lib/LEDToy/src/LEDToy.h
+++ lib/LEDToy/src/LEDToy.h
@@ -1,11 +1,19 @@
 #include <Arduino.h>
 
-class LEDToy
-{
-public:
+class LEDToy {
+ public:
+  // variable to store pin number
   uint8_t pin;
+
+  // constructor
   LEDToy(uint8_t pin_number);
+
+  // called in setup, initializes pin to OUTPUT
   void begin();
+
+  // flashes LED once.
   void flash(int frequency);
+
+  // slowly turns LED on and off once.
   void dim(int delay_time);
 };
